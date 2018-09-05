7120d9d83fb467f0306ad20163a0f61a6f2b75b2
liaohai <liaohai@topshifts.com>
2018-09-05 18:22:40 +0800

    优雅的开发Swift和Objective C混编的Framework

--- DLDHttpClientSDK/API/HttpProgressManager.swift
+++ DLDHttpClientSDK/API/HttpProgressManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import MBProgressHUD
 
 let HttpProgressErrorTag = false
 
@@ -15,73 +16,73 @@ class HttpProgressManager {
 	static let httpProgressWindow = UIWindow.init(frame: UIScreen.main.bounds)
 	static let httpErrorProgressWindow = UIWindow.init(frame: UIScreen.main.bounds)
 	
-//    static let hud = MBProgressHUD()
-//    static let hudError = MBProgressHUD()
+    static let hud = MBProgressHUD()
+    static let hudError = MBProgressHUD()
 	
 	class func showProgressHUD(_ show:Bool, view:UIView? = nil) {
-//        guard show else {
-//            return
-//        }
-//        hud.bezelView.color = UIColor.clear
-//        hud.bezelView.style = .solidColor
-//        hud.contentColor = color_ff473d
-//
-//        if let view = view {
-//            view.addSubview(hud)
-//            hud.show(animated: true)
-//        } else {
-//            httpProgressWindow.addSubview(hud)
-//            hud.show(animated: true)
-//            httpProgressWindow.backgroundColor = UIColor.clear
-//            httpProgressWindow.isHidden = false
-//            httpProgressWindow.makeKeyAndVisible()
-//        }
+        guard show else {
+            return
+        }
+        hud.bezelView.color = UIColor.clear
+        hud.bezelView.style = .solidColor
+        hud.contentColor = UIColor.black
+
+        if let view = view {
+            view.addSubview(hud)
+            hud.show(animated: true)
+        } else {
+            httpProgressWindow.addSubview(hud)
+            hud.show(animated: true)
+            httpProgressWindow.backgroundColor = UIColor.clear
+            httpProgressWindow.isHidden = false
+            httpProgressWindow.makeKeyAndVisible()
+        }
 	}
 	
 	class func hiddenProcessHUD(_ show:Bool, view:UIView? = nil) {
-//        guard show else {
-//            return
-//        }
-//        if let _ = view {
-//            hud.removeFromSuperview()
-//            hud.hide(animated: true)
-//        }else {
-//            hud.hide(animated: true)
-//            httpProgressWindow.resignKey()
-//            httpProgressWindow.isHidden = true
-//        }
+        guard show else {
+            return
+        }
+        if let _ = view {
+            hud.removeFromSuperview()
+            hud.hide(animated: true)
+        }else {
+            hud.hide(animated: true)
+            httpProgressWindow.resignKey()
+            httpProgressWindow.isHidden = true
+        }
 	}
 	
     // interError:内部错误
     class func showErrorProgressHUD(code:Int, interError:Bool = false) {
-//        #if DEBUG
-//        if HttpProgressErrorTag {
-//            hudError.mode = .text
-//            if interError {
-//                hudError.label.text = "业务异常 \(code)"
-//            } else {
-//                hudError.label.text = "服务器异常 \(code)"
-//            }
-//            hudError.label.textColor = UIColor.white
-//            hudError.label.font = UIFont.systemFont(ofSize: 14)
-//
-//            hudError.bezelView.backgroundColor = UIColor.black
-//            hudError.bezelView.alpha = 0.2
-//
-//            httpErrorProgressWindow.addSubview(hudError)
-//            hudError.show(animated: true)
-//            httpErrorProgressWindow.backgroundColor = UIColor.clear
-//            httpErrorProgressWindow.isHidden = false
-//            httpErrorProgressWindow.makeKeyAndVisible()
-//
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-//                // your code here
-//                hudError.hide(animated: true)
-//                httpErrorProgressWindow.resignKey()
-//                httpErrorProgressWindow.isHidden = true
-//            }
-//        }
-//        #endif
+        #if DEBUG
+        if HttpProgressErrorTag {
+            hudError.mode = .text
+            if interError {
+                hudError.label.text = "业务异常 \(code)"
+            } else {
+                hudError.label.text = "服务器异常 \(code)"
+            }
+            hudError.label.textColor = UIColor.white
+            hudError.label.font = UIFont.systemFont(ofSize: 14)
+
+            hudError.bezelView.backgroundColor = UIColor.black
+            hudError.bezelView.alpha = 0.2
+
+            httpErrorProgressWindow.addSubview(hudError)
+            hudError.show(animated: true)
+            httpErrorProgressWindow.backgroundColor = UIColor.clear
+            httpErrorProgressWindow.isHidden = false
+            httpErrorProgressWindow.makeKeyAndVisible()
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                // your code here
+                hudError.hide(animated: true)
+                httpErrorProgressWindow.resignKey()
+                httpErrorProgressWindow.isHidden = true
+            }
+        }
+        #endif
 	}
 	
 }
