e9f296b3599e68f4e7f2f6c5850435b4979d208f
liaohai <liaohai@topshifts.com>
2018-11-29 11:51:04 +0800

    update

--- DLDHttpClientFramework/API/HttpCommend.swift
+++ DLDHttpClientFramework/API/HttpCommend.swift
@@ -6,23 +6,21 @@
 //  Copyright © 2018年 Frank liao. All rights reserved.
 //
 
-import Foundation
-
 // 日志开关
-var isOpenConsoleOutput = true
+var isOpenConsoleOutput:Bool = true
 // 服务器时间差
-var diffientTimeInterval: TimeInterval = 0
+var diffientTimeInterval:TimeInterval = 0
 // header tag缓存
 var headerTagField:String? = nil
 // 强制更新跳转
-var appstoreUrl = ""
+var appstoreUrl:String = ""
 // 强制更新code
-var updateCode = 600
+var updateCode:Int = 600
 
 var reponseError:HttpReponseError? = nil
 
 // 业务code
-var successCode = 0
+var successCode:Int = 3
 
 public class DLDHttpCommend {
     public class func setConsoleOutput(_ open: Bool) {
