7b87176a764539b6c8443473b2e4a9bfb21e0115
liaohai <hn_liaohai@163.com>
2020-09-29 23:01:23 +0800

    编译合并framework

---
+++ Products/DLDHttpClientFramework.framework/Headers/DLDHttpClientFramework.h
@@ -0,0 +1,19 @@
+//
+//  DLDHttpClientSDK.h
+//  DLDHttpClientSDK
+//
+//  Created by Frank liao on 2018/6/27.
+//  Copyright © 2018年 Frank liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DLDHttpClientSDK.
+FOUNDATION_EXPORT double DLDHttpClientSDKVersionNumber;
+
+//! Project version string for DLDHttpClientSDK.
+FOUNDATION_EXPORT const unsigned char DLDHttpClientSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLDHttpClientSDK/PublicHeader.h>
+
+
